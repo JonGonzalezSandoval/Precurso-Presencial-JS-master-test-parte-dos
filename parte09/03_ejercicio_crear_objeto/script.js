c2e96f7843d94775cf75feae7bd6220bef02bed6
jongonzalezsandoval <jongonzalezsandoval@gmail.com>
2023-09-12 19:04:34 +0200

    Ejercicios Tanda 09 terminados, repaso al último alineado vertical pendiente

---
+++ parte09/03_ejercicio_crear_objeto/script.js
@@ -0,0 +1,16 @@
+let coche = {
+    color: "Rojo",
+    marca: "Ford",
+    modelo: "Focus",
+    motor: { 
+        cilindrios: 4, 
+        capacidad: 2.2,
+    },
+}
+
+
+console.log("Color: " + coche.color);
+console.log("Marca: " + coche.marca);
+console.log("Modelo: " + coche.modelo);
+console.log("Cilindros: " + coche.motor.cilindrios);
+console.log("Capacidad: " + coche.motor.capacidad);
---
+++ parte09/07_ejercicio_añadir_atributos/script.js
@@ -0,0 +1,11 @@
+let persona = {
+    nombre: "Jon",
+    edad: 25,
+}
+
+console.log(persona.nombre);
+console.log(persona.edad);
+
+persona.altura = 1.8;
+
+console.log(persona.altura);
